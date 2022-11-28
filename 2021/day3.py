bfd7f6740fd74089f0279acfd373fdf1f3f28cdf
vivello <56002394+vivello@users.noreply.github.com>
2022-11-28 01:17:13 -0500

    Add part 2 solution to day 3

--- 2021/day3.py
+++ 2021/day3.py
@@ -2,7 +2,7 @@
 # Calculate gamma and epsilon rate of submarine, then multiply for power consumption rate
 # 4174964
 
-from collections import defaultdict
+from collections import Counter
 
 def power_consumption(file_name):
     with open(file_name, "r") as f:
@@ -28,6 +28,37 @@ def power_consumption(file_name):
         epsilon= int(''.join(epsilon),2)
 
         return gamma * epsilon
+
+
+# Part 2
+# Calculate oxygen generator rating and CO2 scrubber rating to find life support rating
+# 4474944
+
+def diag_filter(dlist, default):
+    index = 0
+    guide = Counter(list(map(lambda x: x[0], dlist)))
+    while len(dlist) > 1:
+        if default == 1:
+            if guide["0"] > guide["1"]:
+                target = "0"
+            else:
+                target = "1"
+        else:
+            if guide["1"] < guide["0"]:
+                target = "1"
+            else:
+                target = "0"
+        dlist = list(filter(lambda x: x[index] == target, dlist))
+        index += 1
+        guide = Counter(list(map(lambda x: x[index], dlist)))
+    return dlist[0]
+                
+def life_support(file_name):
+    with open(file_name, "r") as f:
+        diagnostics = f.readlines()
+        oxygen = int(diag_filter(diagnostics, 1),2)
+        co2 = int(diag_filter(diagnostics, 0),2)
+    return oxygen * co2
+        
+
     
-                
-                

b10363151f6605861480f7a1c33caf82328dd16a
vivello <56002394+vivello@users.noreply.github.com>
2022-12-02 19:09:19 -0500

    Update with day 2 part 2 solution

--- 2022/day2.py
+++ 2022/day2.py
@@ -9,6 +9,7 @@ def rpc(rpc_sets):
            "X": "B",
            "Y": "C",
            "Z": "A"}
+
     choice_points = {"X": 1,
                      "Y": 2,
                      "Z": 3}
@@ -24,8 +25,31 @@ def rpc(rpc_sets):
 
     return score
 
+# Part 2
+# Calculate total score according to strategy guide with updated meaning
+# 10835
+
+def rpc_strat(rpc_sets):
+    win = {"A": ("Y","X","Z"),
+           "B": ("Z","Y","X"),
+           "C": ("X","Z","Y"),
+           "Z": (0,6),
+           "Y": (1,3),
+           "X": (2,0)}
+
+    choice_points = {"X": 1,
+                     "Y": 2,
+                     "Z": 3}
+    score = 0
+
+    for game in rpc_sets:
+        score += win[game[1]][1]
+        score += choice_points[win[game[0]][win[game[1]][0]]]
+
+    return score
+
 def main(file_name):
     with open(file_name,"r") as f:
         tourney = [x.strip().split() for x in f.readlines()]
 
-        return rpc(tourney)
+        return rpc_strat(tourney)

da30e39ac042891047f8097cfc3244c1fb6850f4
vivello <56002394+vivello@users.noreply.github.com>
2022-12-04 00:49:18 -0500

    Add day 4 input and part 1 solution

---
+++ 2022/day4.py
@@ -0,0 +1,26 @@
+# Part 1
+# Count wholly overlapping range pairs
+# 571
+
+def count_overlap(elf_pairs):
+    count = 0
+
+    for pair in elf_pairs:
+        if pair[0][0] >= pair[1][0] and pair[0][1] <= pair[1][1]:
+            count += 1
+        elif pair[1][0] >= pair[0][0] and pair[1][1] <= pair[0][1]:
+            count += 1
+
+    return count
+
+def make_pretty(ep):
+    return [[int(y) for  y in x.split('-')] for x in ep.strip().split(',')]
+
+def main(file_name):
+    with open(file_name) as f:
+        e_pairs = f.readlines()
+
+        return count_overlap([make_pretty(x) for x in e_pairs])
+        
+
+        
